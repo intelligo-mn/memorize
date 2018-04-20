0501a2cebefe39a96fd40ac24290308590553cb1
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2018-04-20 17:15:29 +0900

    Create word table

---
+++ app/src/main/java/cloud/techstar/jisho/database/WordTable.java
@@ -0,0 +1,153 @@
+package cloud.techstar.jisho.database;
+
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cloud.techstar.jisho.models.Words;
+
+public class WordTable {
+
+    public WordTable() {
+        Words word = new Words();
+    }
+
+    public static String create(){
+        return "CREATE TABLE "+ Words.TABLE_WORDS+" (" +
+                Words.WORDS_ID + " TEXT PRIMARY KEY," +
+                Words.WORDS_CHARACTER + " TEXT," +
+                Words.WORDS_MEANING + " TEXT," +
+                Words.WORDS_KANJI + " TEXT," +
+                Words.WORDS_LEVEL + " TEXT," +
+                Words.WORDS_ISMEMORIZE + " TEXT);";
+    }
+
+    public void insert(Words word) {
+        if (word == null) {
+            return;
+        }
+        SQLiteDatabase db = DatabaseManager.getInstance().openDatabase();
+        if (db == null) {
+            return;
+        }
+        db.beginTransaction();
+        try {
+            ContentValues cv = new ContentValues();
+            cv.put(Words.WORDS_ID, word.getId());
+            cv.put(Words.WORDS_CHARACTER, word.getCharacter());
+            cv.put(Words.WORDS_MEANING, word.getMeaning());
+            cv.put(Words.WORDS_KANJI, word.getKanji());
+            cv.put(Words.WORDS_LEVEL, word.getLevel());
+            cv.put(Words.WORDS_ISMEMORIZE, word.getIsMemorize());
+            db.insert(Words.TABLE_WORDS, null, cv);
+            db.setTransactionSuccessful();
+        } catch (Exception ex){
+            ex.printStackTrace();
+        } finally {
+            db.endTransaction();
+            DatabaseManager.getInstance().closeDatabase();
+        }
+    }
+
+    public Words select(int id) {
+        SQLiteDatabase db = DatabaseManager.getInstance().openDatabase();
+        if (db == null) {
+            return null;
+        }
+        Cursor cursor = db.query(Words.TABLE_WORDS, new String[]{
+                        Words.WORDS_ID,
+                        Words.WORDS_CHARACTER,
+                        Words.WORDS_MEANING,
+                        Words.WORDS_KANJI,
+                        Words.WORDS_LEVEL,
+                        Words.WORDS_ISMEMORIZE
+                }, Words.WORDS_ID + "=?",
+                new String[]{String.valueOf(id)}, null, null, null, null);
+        if (!cursor.moveToFirst()) {
+            return null;
+        }
+
+        Words word = new Words();
+        word.setId(cursor.getString(Words.WORDS_ID_INDEX));
+        word.setCharacter(cursor.getString(Words.WORDS_CHARACTER_INDEX));
+        word.setMeaning(cursor.getString(Words.WORDS_MEANING_INDEX));
+        word.setKanji(cursor.getString(Words.WORDS_KANJI_INDEX));
+        word.setLevel(cursor.getString(Words.WORDS_LEVEL_INDEX));
+        word.setIsMemorize(cursor.getString(Words.WORDS_ISMEMORIZE_INDEX));
+        cursor.close();
+        DatabaseManager.getInstance().closeDatabase();
+        return word;
+    }
+
+    public List<Words> selectAll() {
+        List<Words> words = new ArrayList<>();
+        String selectQuery = "SELECT * FROM " + Words.TABLE_WORDS;
+        SQLiteDatabase db = DatabaseManager.getInstance().openDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        if (cursor.moveToFirst()) {
+            do {
+                Words word = new Words();
+                word.setId(cursor.getString(Words.WORDS_ID_INDEX));
+                word.setCharacter(cursor.getString(Words.WORDS_CHARACTER_INDEX));
+                word.setMeaning(cursor.getString(Words.WORDS_MEANING_INDEX));
+                word.setKanji(cursor.getString(Words.WORDS_KANJI_INDEX));
+                words.add(word);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        DatabaseManager.getInstance().closeDatabase();
+        return words;
+    }
+
+    public int update(Words word) {
+        if (word == null) {
+            return -1;
+        }
+        SQLiteDatabase db = DatabaseManager.getInstance().openDatabase();
+        if (db == null) {
+            return -1;
+        }
+        ContentValues cv = new ContentValues();
+        cv.put(Words.WORDS_ID, word.getId());
+        cv.put(Words.WORDS_CHARACTER, word.getCharacter());
+        cv.put(Words.WORDS_MEANING, word.getMeaning());
+        cv.put(Words.WORDS_KANJI, word.getKanji());
+        int rowCount = db.update(Words.TABLE_WORDS, cv, Words.WORDS_ID + "=?",
+                new String[]{String.valueOf(word.getId())});
+        DatabaseManager.getInstance().closeDatabase();
+        return rowCount;
+    }
+
+    public void delete(Words word) {
+        if (word == null) {
+            return;
+        }
+        SQLiteDatabase db = DatabaseManager.getInstance().openDatabase();
+        if (db == null) {
+            return;
+        }
+        db.delete(Words.TABLE_WORDS, Words.WORDS_ID + "=?", new String[]{String.valueOf(word.getId())});
+        DatabaseManager.getInstance().closeDatabase();
+    }
+
+    public void deleteAll()
+    {
+        SQLiteDatabase db = DatabaseManager.getInstance().openDatabase();
+        db.delete(Words.TABLE_WORDS, null, null);
+        DatabaseManager.getInstance().closeDatabase();
+    }
+
+    public int count() {
+        String query = "SELECT * FROM  " + Words.TABLE_WORDS;
+        SQLiteDatabase db = DatabaseManager.getInstance().openDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        int count = cursor.getCount();
+        cursor.close();
+        DatabaseManager.getInstance().closeDatabase();
+        return count;
+    }
+}
--- app/src/main/java/cloud/techstar/jisho/models/Words.java
+++ app/src/main/java/cloud/techstar/jisho/models/Words.java
@@ -1,17 +1,33 @@
 package cloud.techstar.jisho.models;
 
 public class Words {
-    String id;
-    String character;
-    String meaning;
-    String kanji;
-    String level;
-    boolean isMemorize;
+
+    public static final String TABLE_WORDS       = "words";
+    public static final String WORDS_ID          = "id";
+    public static final String WORDS_CHARACTER   = "character";
+    public static final String WORDS_MEANING     = "meaning";
+    public static final String WORDS_KANJI       = "kanji";
+    public static final String WORDS_LEVEL       = "level";
+    public static final String WORDS_ISMEMORIZE  = "ismemorize";
+
+    public static final int WORDS_ID_INDEX         = 0;
+    public static final int WORDS_CHARACTER_INDEX  = 1;
+    public static final int WORDS_MEANING_INDEX    = 2;
+    public static final int WORDS_KANJI_INDEX      = 3;
+    public static final int WORDS_LEVEL_INDEX      = 4;
+    public static final int WORDS_ISMEMORIZE_INDEX = 5;
+
+    private String id;
+    private String character;
+    private String meaning;
+    private String kanji;
+    private String level;
+    private String isMemorize;
 
     public Words() {
     }
 
-    public Words(String id, String character, String meaning, String kanji, String level, boolean isMemorize) {
+    public Words(String id, String character, String meaning, String kanji, String level, String isMemorize) {
         this.id = id;
         this.character = character;
         this.meaning = meaning;
@@ -60,11 +76,11 @@ public class Words {
         this.level = level;
     }
 
-    public boolean isMemorize() {
+    public String getIsMemorize() {
         return isMemorize;
     }
 
-    public void setMemorize(boolean memorize) {
-        isMemorize = memorize;
+    public void setIsMemorize(String isMemorize) {
+        this.isMemorize = isMemorize;
     }
 }
