d951b8dab83c955b7b005fcdc5c74d34f4efba60
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2018-04-24 17:50:17 +0900

    get data server saved to local database

--- app/src/main/java/cloud/techstar/jisho/activity/SplashActivity.java
+++ app/src/main/java/cloud/techstar/jisho/activity/SplashActivity.java
@@ -21,6 +21,7 @@ import cloud.techstar.jisho.R;
 import cloud.techstar.jisho.Utils.ConnectionDetector;
 import cloud.techstar.jisho.Utils.JishoConstant;
 import cloud.techstar.jisho.database.WordTable;
+import cloud.techstar.jisho.models.Words;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.MultipartBody;
@@ -82,20 +83,32 @@ public class SplashActivity extends AppCompatActivity {
                     public void run() {
 
                         try {
-                            JSONArray ob = new JSONArray(String.valueOf(res));
-                            if (ob.length() < 1)
-                                return;
 
-                            JSONObject words = ob.getJSONObject(0);
+                            JSONObject ob = new JSONObject(res);
+                            JSONArray memorize = ob.getJSONArray("memorize");
 
-                            Logger.json(words.toString());
+                            Logger.json(memorize.toString());
 
-                            if (words.length() > 0) {
+                            if (memorize.length() > 0) {
+                                WordTable wordTable = new WordTable();
 
+                                wordTable.deleteAll();
+
+                                for (int i = 0; i < memorize.length(); i++) {
+                                    Words words = new Words();
+                                    words.setCharacter(memorize.getJSONObject(i).getString("character"));
+                                    words.setMeaning(memorize.getJSONObject(i).getString("meanings"));
+                                    words.setKanji(memorize.getJSONObject(i).getString("kanji"));
+                                    words.setIsMemorize("false");
+                                    words.setLevel(memorize.getJSONObject(i).getString("level"));
+                                    wordTable.insert(words);
+                                }
                             } else {
                                 Toast.makeText(SplashActivity.this, "Words not found", Toast.LENGTH_LONG)
                                         .show();
                             }
+                            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                            finish();
                         } catch (JSONException e) {
                             e.printStackTrace();
                         } catch (Exception ex) {
