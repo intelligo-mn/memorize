7dcc63924e413a970c5386c98cbc95d6d18a8645
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2018-04-24 18:05:38 +0900

    Create adapter

--- app/src/main/java/cloud/techstar/jisho/adapter/WordListAdapter.java
+++ app/src/main/java/cloud/techstar/jisho/adapter/WordListAdapter.java
@@ -1,4 +1,66 @@
 package cloud.techstar.jisho.adapter;
 
-public class WordListAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import cloud.techstar.jisho.R;
+import cloud.techstar.jisho.models.Words;
+
+public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.ViewHolder> {
+    private final Context context;
+    private List<Words> words;
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private ImageView imageView;
+        private CardView cardView;
+        private TextView characterText;
+        private TextView meaningText;
+        private ViewHolder(View v) {
+            super(v);
+            v.setOnClickListener(this);
+
+            cardView = v.findViewById(R.id.user_card_view);
+            characterText = v.findViewById(R.id.character_text);
+            meaningText = v.findViewById(R.id.meaning_text);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+        }
+    }
+
+    public WordListAdapter(Context context, List<Words> words) {
+        this.context = context;
+        this.words = words;
+    }
+
+    @Override
+    public WordListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                         int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.word_recycler_item, parent, false);
+        WordListAdapter.ViewHolder vh = new WordListAdapter.ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(WordListAdapter.ViewHolder holder, int position) {
+        holder.characterText.setText(words.get(position).getCharacter());
+        holder.meaningText.setText(words.get(position).getMeaning());
+    }
+
+    @Override
+    public int getItemCount() {
+        return words.size();
+    }
 }
--- app/src/main/java/cloud/techstar/jisho/fragments/SearchFragment.java
+++ app/src/main/java/cloud/techstar/jisho/fragments/SearchFragment.java
@@ -2,14 +2,28 @@ package cloud.techstar.jisho.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
+import cloud.techstar.jisho.AppMain;
 import cloud.techstar.jisho.R;
+import cloud.techstar.jisho.adapter.WordListAdapter;
+import cloud.techstar.jisho.database.WordTable;
+import cloud.techstar.jisho.models.Words;
 
 public class SearchFragment extends Fragment {
 
+    private WordTable wordTable;
+    private Handler mHandler;
+    List<Words> words;
     public static android.app.Fragment newInstance() {
         SearchFragment fragment = new SearchFragment();
         return fragment;
@@ -18,12 +32,24 @@ public class SearchFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        wordTable = new WordTable();
+        mHandler = new Handler(Looper.getMainLooper());
+        words = wordTable.selectAll();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_search, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+
+        final RecyclerView mRecyclerView = root.findViewById(R.id.word_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(AppMain.getContext());
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        RecyclerView.Adapter mAdapter = new WordListAdapter(AppMain.getContext(), words);
+        mRecyclerView.setAdapter(mAdapter);
+        return root;
     }
 }
