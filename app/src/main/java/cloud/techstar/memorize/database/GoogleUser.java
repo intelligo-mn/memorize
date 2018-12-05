fbc97e0a61c11512dcc811a48f9702b7dc9efd54
Turtuvshin Byambaa <toroo.byamba@gmail.com>
2018-12-05 15:34:37 +0900

    Created user models

---
+++ app/src/main/java/cloud/techstar/memorize/database/GoogleUser.java
@@ -0,0 +1,34 @@
+package cloud.techstar.memorize.database;
+
+public class GoogleUser {
+    private String displayName;
+    private String photoUrl;
+    private String idToken;
+
+    public GoogleUser() {
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public String getIdToken() {
+        return idToken;
+    }
+
+    public void setIdToken(String idToken) {
+        this.idToken = idToken;
+    }
+}
